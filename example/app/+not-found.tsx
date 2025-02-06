94ea2e99ac7f361d9e17a829f0452d7f30ad5c2e
du.nguyen <du.nguyen@asnet.com.vn>
2025-02-06 10:14:18 +0700

    init template

--- example/app/+not-found.tsx
+++ example/app/+not-found.tsx
@@ -1,12 +1,40 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { Link, Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
 
-const NotFoundScreen = () => {
+import { Text, View } from '@/components/Themed';
+
+export default function NotFoundScreen() {
   return (
-    <View>
-      <Text>+not-found</Text>
-    </View>
-  );
-};
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <View style={styles.container}>
+        <Text style={styles.title}>This screen doesn't exist.</Text>
 
-export default NotFoundScreen;
+        <Link href="/" style={styles.link}>
+          <Text style={styles.linkText}>Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
