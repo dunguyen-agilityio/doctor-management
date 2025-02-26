9bad0c4407771963e469306186987d3d3e5ce1e3
Du Huu Nguyen <du.nguyen@asnet.com.vn>
2025-02-26 16:21:04 +0700

    Revert "Merge branch 'fix/show-splash-screen' into 'main'"
    This reverts merge request !50

--- example/components/common/Button/Button.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react-native";
-import Button from ".";
-
-test("basic test", () => {
-  const component = render(<Button />);
-  //   expect(screen.getAllByRole("button")).toBeOnTheScreen();
-  expect(component.toJSON()).toMatchSnapshot();
-});
