a5a1b38742ebc02be82731ae9a64015ac3261247
du.nguyen <du.nguyen@asnet.com.vn>
2025-03-18 10:06:52 +0700

    Fix: rename root folder

--- practice-one/src/constants/dimensions.ts
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export const WINDOW = Dimensions.get('window');
-export const SCREEN = Dimensions.get('screen');
-export const WINDOW_WIDTH = WINDOW.width;
-export const WINDOW_HEIGHT = WINDOW.height;
--- practice-one/src/constants/screens.ts
+++
@@ -1,8 +0,0 @@
-export enum ROUTES {
-  HOME = 'Home-Screen',
-  SEARCH = 'Search-Screen',
-  DETAIL = 'Detail-Screen',
-  FAVORITE = 'Favorite-Screen',
-  ROOT = 'Root-Screen',
-  SPLASH = 'SPLASH-Screen',
-}
