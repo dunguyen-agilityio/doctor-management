ad7912b0748f8142252b09b2f6a74062d46f2c34
du.nguyen <du.nguyen@asnet.com.vn>
2025-03-18 10:04:39 +0700

    Fix: Remove redundant code

--- practice-one/src/constants/api.ts
+++ practice-one/src/constants/api.ts
@@ -2,7 +2,6 @@ import Constants from 'expo-constants';
 
 export const API_ENTITIES = {
   FOOD_LIST: 'food-list',
-  FAVORITES: 'favorites',
 };
 
 export const API_ENDPOINT = Constants.expoConfig?.extra?.apiEndpoint;
