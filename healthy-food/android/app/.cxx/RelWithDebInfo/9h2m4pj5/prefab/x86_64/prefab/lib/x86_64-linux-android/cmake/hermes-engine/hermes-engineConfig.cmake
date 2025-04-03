5f0f4f011a39fce212d183fb29f4006c7b806bed
du.nguyen <du.nguyen@asnet.com.vn>
2025-04-03 10:13:43 +0700

    Fix: Merge from main

---
+++ healthy-food/android/app/.cxx/RelWithDebInfo/9h2m4pj5/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/home/dunguyen/.gradle/caches/8.10.2/transforms/2801946c0c1cb8512afdb6610c0b7f4a/transformed/hermes-android-0.76.8-release/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/dunguyen/.gradle/caches/8.10.2/transforms/2801946c0c1cb8512afdb6610c0b7f4a/transformed/hermes-android-0.76.8-release/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
