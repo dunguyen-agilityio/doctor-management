58b47a61e8e15bbd77dc06584dc0d1a687fcedd0
du.nguyen <du.nguyen@asnet.com.vn>
2025-05-14 15:06:43 +0700

    Feat: Config auto sort import

--- health-pal/eslint.config.js
+++ health-pal/eslint.config.js
@@ -2,11 +2,15 @@
 const { defineConfig } = require('eslint/config')
 const expoConfig = require('eslint-config-expo/flat')
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
+const sortImport = require('eslint-plugin-import')
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*', '/.expo', 'node_modules', '.storybook'],
+    plugins: {
+      import: sortImport,
+    },
   },
 ])
