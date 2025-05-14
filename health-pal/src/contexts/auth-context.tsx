3f1358bb6849143cd420829a26b1a62998d09620
du.nguyen <du.nguyen@asnet.com.vn>
2025-05-14 18:16:09 +0700

    Feat: Define Auth store

---
+++ health-pal/src/contexts/auth-context.tsx
@@ -0,0 +1,48 @@
+import { useStorageState } from '@app/hooks/useStorageState'
+
+import { type PropsWithChildren, createContext, use } from 'react'
+
+const AuthContext = createContext<{
+  signIn: () => void
+  signOut: () => void
+  session?: string | null
+  isLoading: boolean
+}>({
+  signIn: () => null,
+  signOut: () => null,
+  session: null,
+  isLoading: false,
+})
+
+// This hook can be used to access the user info.
+export function useSession() {
+  const value = use(AuthContext)
+  if (!value) {
+    throw new Error('useSession must be wrapped in a <SessionProvider />')
+  }
+
+  return value
+}
+
+export function SessionProvider({ children }: PropsWithChildren) {
+  const [[isLoading, session], setSession] = useStorageState('session')
+
+  console.log('session', session)
+
+  return (
+    <AuthContext
+      value={{
+        signIn: () => {
+          // Perform sign-in logic here
+          setSession('xxx')
+        },
+        signOut: () => {
+          setSession(null)
+        },
+        session,
+        isLoading,
+      }}>
+      {children}
+    </AuthContext>
+  )
+}
